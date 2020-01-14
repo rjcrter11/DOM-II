c2e92e8c36bd95d1803e8401ddf0b0e87ac3ea4b
rjcrter11 <rjcrter11@gmail.com>
2020-01-14 14:44:57 -0600

    did the first couple events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+const imgTransform = document.querySelectorAll("img");
+
+imgTransform.forEach((el) => {
+  el.addEventListener("mouseenter", (event) => {
+    el.style.transform = "scale(1.2)";
+    el.style.transition = "transform 0.5s";
+  });
+});
+
+imgTransform.forEach((el) => {
+  el.addEventListener("mouseleave", (event) => {
+    el.style.transform = "scale(1)";
+  });
+});
+
+const funBusLogo = document.querySelector(".logo-heading");
+
+funBusLogo.addEventListener("click", () => {
+  gsap.to(".logo-heading", {
+    duration: 1,
+    rotationY: 360,
+    yoyo: true,
+    repeat: 1
+  });
+});
+
+const newBus = document.querySelector("img");
+newBus.addEventListener("resize", () => {
+  newBus.src = "img/bus.jpg";
+});
