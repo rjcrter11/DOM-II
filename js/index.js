81e1d1741b59a29495af8e5a97ae970768a42128
rjcrter11 <rjcrter11@gmail.com>
2020-01-14 16:51:43 -0600

    met mvp

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const imgTransform = document.querySelectorAll("img");
 imgTransform.forEach((el) => {
   el.addEventListener("mouseenter", (event) => {
     el.style.transform = "scale(1.2)";
-    el.style.transition = "transform 0.5s";
+    el.style.transition = "transform  0.5s ";
   });
 });
 
@@ -38,6 +38,7 @@ window.addEventListener("resize", () => {
   newBus.src = "img/bus.jpg";
 });
 
+// ----Scrolls to footer on double click-------
 const scrollAnchors = document.querySelectorAll("a");
 const footer = document.querySelector("footer");
 const footerY = footer.getBoundingClientRect().top;
@@ -49,7 +50,67 @@ scrollAnchors.forEach((el) => {
   });
 });
 
-// const scrollToFooter = (event) => {
-//   window.scrollBy(0, footerY);
-//   event.preventDefault;
-// }
+//----Underlines and changes anchor colors on hover-----
+const animateAnchors = document.querySelectorAll("a");
+animateAnchors.forEach((el) => {
+  el.addEventListener("mouseover", (event) => {
+    el.style.color = "blue";
+    el.style.borderBottom = "2px solid blue";
+    el.style.transition = " transform 0.5s ";
+    el.style.fontSize = "2.5rem";
+  });
+});
+
+//------Reverts changes to anchors-------
+animateAnchors.forEach((el) => {
+  el.addEventListener("mouseleave", (event) => {
+    el.style.color = "black";
+    el.style.borderBottom = "none";
+    el.style.fontSize = "1.6rem";
+  });
+});
+
+// -----Changes card color on click------
+const destinations = document.querySelectorAll(".destination");
+destinations.forEach((el) => {
+  el.addEventListener("click", (event) => {
+    el.style.backgroundColor = "#FFEBCD";
+  });
+});
+// ----Changes button color on click, stops propagation-----
+const clickButtons = document.querySelectorAll(".btn");
+clickButtons.forEach((el) => {
+  el.addEventListener("click", (event) => {
+    el.style.backgroundColor = "#212529";
+    el.style.color = "skyblue";
+    el.style.marginLeft = "8%";
+    event.stopPropagation();
+  });
+});
+
+// --------blurs and unblurs imgages---------
+
+const unblur = document.querySelectorAll("img");
+const blur = document.querySelectorAll("img");
+
+blur.forEach((el) => {
+  el.style.filter = "blur(2px)";
+});
+
+unblur.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    el.style.filter = "none";
+  });
+});
+
+// --------footer color change-------
+const footerp = document.querySelector(".footer p");
+const footerColor = document.querySelector("footer");
+
+footerColor.addEventListener("mouseenter", () => {
+  footerColor.style.backgroundColor = "#212529";
+  footerColor.style.borderTop = "none";
+});
+footerp.addEventListener("mouseover", () => {
+  footerp.style.color = "#17A2B8";
+});
